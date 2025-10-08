b28254b9804f75b822915ad26c67c19a0e19d587
Felipe Barreto <felipebarreto00000@gmail.com>
2025-10-08 00:41:40 -0300

    feat: update Header component to make it responsive

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Aplicación web - E-commerce
+# Aplicación web responsive y accesible - E-commerce
 
-Aplicación web de e-commerce que permite a los usuarios ver los productos de la tienda, filtrarlos por categoría, consultar la información detallada de cada uno y agregarlos al carrito. En el carrito se listan los productos seleccionados, y se puede incrementar o reducir la cantidad de unidades de cada uno; además de mostrarse el precio total de la compra.
+La aplicación permite a los usuarios ver los productos de la tienda, filtrarlos por categoría, consultar la información detallada de cada uno y agregarlos al carrito. En el carrito se listan los productos seleccionados, y se puede incrementar o reducir la cantidad de unidades de cada uno; además de mostrarse el precio total de la compra.
 
 <br>
 
 ## Para su construcción se utilizaron las siguientes herramientas y tecnologías:
 
-- React con TypeScript.
-
 - Vite para la creación y configuración inicial del proyecto.
+
+- React para crear la interfaz de usuario a partir de componentes.
+
+- TypeScript para detectar errores durante el desarrollo.
 
 - Tailwind CSS para los estilos.
 
@@ -16,9 +18,9 @@ Aplicación web de e-commerce que permite a los usuarios ver los productos de la
 
 - React Redux y Redux Toolkit para el manejo del estado global.
 
-- Git para control de versiones.
+- Git para el control de versiones.
 
-- GitHub para alojamiento del repositorio.
+- GitHub para el alojamiento del repositorio.
 
 <br>
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className="fixed w-full bg-gradient-to-b from-gray-950 to-gray-700 py-7">
-      <nav>
-        <ul className="flex justify-center md:justify-between">
+      <nav className="flex justify-between flex-row-reverse lg:block" aria-label="Secciones de la página">
+        {/* Menu for large screens */}
+        <ul className="hidden lg:flex justify-between">
           <li>
             <NavLink
               to="/productos"
@@ -17,56 +21,48 @@ const Header = () => {
               Inicio
             </NavLink>
           </li>
-          <div className="flex">
-            <li>
-              <NavLink
-                to="/productos/hombre"
-                end
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
-                    : "text-2xl font-bold text-white px-8 hover:underline"
-                }>
-                Hombre
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/productos/mujer"
-                end
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
-                    : "text-2xl font-bold text-white px-8 hover:underline"
-                }>
-                Mujer
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/productos/joyeria"
-                end
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
-                    : "text-2xl font-bold text-white px-8 hover:underline"
-                }>
-                Joyería
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/productos/electronica"
-                end
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
-                    : "text-2xl font-bold text-white px-8 hover:underline"
-                }>
-                Electrónica
-              </NavLink>
-            </li>
-          </div>
+          <li className="flex">
+            <NavLink
+              to="/productos/hombre"
+              end
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
+                  : "text-2xl font-bold text-white px-8 hover:underline"
+              }>
+              Hombre
+            </NavLink>
+            <NavLink
+              to="/productos/mujer"
+              end
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
+                  : "text-2xl font-bold text-white px-8 hover:underline"
+              }>
+              Mujer
+            </NavLink>
+            <NavLink
+              to="/productos/joyeria"
+              end
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
+                  : "text-2xl font-bold text-white px-8 hover:underline"
+              }>
+              Joyería
+            </NavLink>
+            <NavLink
+              to="/productos/electronica"
+              end
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl font-bold text-blue-300 px-8 hover:underline"
+                  : "text-2xl font-bold text-white px-8 hover:underline"
+              }>
+              Electrónica
+            </NavLink>
+          </li>
           <li>
             <NavLink
               to="/carrito"
@@ -79,6 +75,112 @@ const Header = () => {
             </NavLink>
           </li>
         </ul>
+
+        {/* Hamburger button for small screens */}
+        <button
+          className="flex justify-center items-center w-8 h-8 mr-2 cursor-pointer group lg:hidden"
+          aria-controls="small-screen-navigation"
+          aria-expanded={open}
+          aria-label={open ? "Cerrar menú" : "Abrir menú"}
+          onClick={() => setOpen((state) => !state)}>
+          <span
+            className={[
+              "relative block w-[22px] h-[2px] bg-gray-50",
+
+              "before:absolute before:left-0 before:top-[-7px] before:w-[22px] before:h-[2px] before:bg-gray-50 before:duration-200 group-hover:before:bg-blue-300",
+
+              "after:absolute after:left-0 after:bottom-[-7px] after:w-[22px] after:h-[2px] after:bg-gray-50 after:duration-200 group-hover:after:bg-blue-300",
+
+              open
+                ? "bg-transparent before:origin-left before:rotate-41 after:origin-left after:-rotate-41"
+                : "duration-200 group-hover:bg-blue-300",
+            ].join(" ")}
+            aria-hidden="true"
+          />
+        </button>
+
+        {/* Menu for small screens */}
+        {open && (
+          <ul id="small-screen-navigation" className="ml-4 lg:hidden">
+            <li>
+              <NavLink
+                to="/productos"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Inicio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/productos/hombre"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Hombre
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/productos/mujer"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Mujer
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/productos/joyeria"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Joyería
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/productos/electronica"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Electrónica
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/carrito"
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-2xl font-bold text-blue-300 hover:underline"
+                    : "text-2xl font-bold text-white hover:underline"
+                }
+                onClick={() => setOpen(false)}>
+                Carrito
+              </NavLink>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
